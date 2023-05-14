e3967f33c2ccfb8e049bef1774e409312ddda31b
= <=>
2023-05-14 07:57:00 +0530

    INitial Commit

---
+++ Employee/Data/EmpDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employee.Models.Domain;
+namespace Employee.Data
+{
+    public class EmpDBContextL : DbContext
+    {
+        protected EmpDBContextL(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Employee/Models/Domain/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee.Models.Domain
+{
+    public class Employee
+    {
+
+        public Guid EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public string Email { get; set; }
+        public long Mobile { get; set; }
+    }
+}
